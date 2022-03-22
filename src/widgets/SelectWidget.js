3e6174b2a68b2afca03c66d4652438c943e9d077
Bogdan Dumitrescu <bogdoomy@gmail.com>
2022-03-22 17:53:41 +0000

    Add select all/none buttons

--- src/widgets/SelectWidget.js
+++ src/widgets/SelectWidget.js
@@ -6,13 +6,28 @@ import TagWidget from "./TagWidget";
 export default class SelectWidget extends React.Component {
     constructor(props) {
         super(props)
-        this.state= {
-            selectedCows: ["moo A", "moo B"],
+        this.state = {
+            selectedCows: [],
             availableCows: ["moo", "moo 2", "moo 3", "moo 4", "moo 5", "moo 6", "moo 7", "moo 8", "moo 9", "moo 10", "moo 11", "moo 12", "moo 13", "moo 14", "moo 15", "moo 16", "moo 17"],
             searchedCows: ["moo", "moo 2", "moo 3", "moo 4", "moo 5", "moo 6", "moo 7", "moo 8", "moo 9", "moo 10", "moo 11", "moo 12", "moo 13", "moo 14", "moo 15", "moo 16", "moo 17"],
             searchText: ''
         }
+        
+        var enableAll = 0, enableNone = 0;
 
+        if (this.state.availableCows.length !== 0)
+            enableAll = 1;
+        if (this.state.selectedCows.length !== 0)
+            enableNone = 1;
+
+        this.state = {
+            selectedCows: [],
+            availableCows: ["moo", "moo 2", "moo 3", "moo 4", "moo 5", "moo 6", "moo 7", "moo 8", "moo 9", "moo 10", "moo 11", "moo 12", "moo 13", "moo 14", "moo 15", "moo 16", "moo 17"],
+            searchedCows: ["moo", "moo 2", "moo 3", "moo 4", "moo 5", "moo 6", "moo 7", "moo 8", "moo 9", "moo 10", "moo 11", "moo 12", "moo 13", "moo 14", "moo 15", "moo 16", "moo 17"],
+            searchText: '',
+            enableSelectAll: enableAll,
+            enableSelectNone: enableNone
+        }
     }
 
     handleSearchTextChange(event) {
@@ -22,6 +37,14 @@ export default class SelectWidget extends React.Component {
     }
 
     updateSelectedCows(val) {
+        console.log(this.state.availableCows)
+        var enableAll = 0, enableNone = 0;
+
+        if (this.state.availableCows.length !== 0)
+            enableAll = 1;
+        if (this.state.selectedCows.length !== 0)
+            enableNone = 1;
+
         var cows = []
         for (var i = 0; i < this.state.availableCows.length; i++) {
             var cow = this.state.availableCows[i]
@@ -30,14 +53,20 @@ export default class SelectWidget extends React.Component {
             }
         }
         // var sortedCows = cows.sort()
-        this.setState({searchedCows: cows})
+        this.setState({
+            searchedCows: cows,
+            enableSelectAll: enableAll,
+            enableSelectNone: enableNone
+        })
     }
 
     displaySelectedCows() {
         var selectedCowRows = [];
         var keyInc = 0;
         for (var cow in this.state.selectedCows) {
-            selectedCowRows.push(<TagWidget name ={this.state.selectedCows[cow]} 
+            selectedCowRows.push(<TagWidget name = {this.state.selectedCows[cow]}
+                                            enableSelectAll = {this.state.enableSelectAll}
+                                            enableSelectNone = {this.state.enableSelectNone}
                                             passedKey={keyInc} 
                                             key={cow} 
                                             onClick={(e) => this.handleSelectedCowClick(e)}/>)
@@ -73,7 +102,6 @@ export default class SelectWidget extends React.Component {
             selectedCows: newSelectedCows
         }, this.updateSelectedCows(this.state.searchText))
     }
-
     
     handleSelectedCowClick(e) {
         var name = e.target.innerText;
@@ -90,26 +118,60 @@ export default class SelectWidget extends React.Component {
         }, this.updateSelectedCows(this.state.searchText))
     }
 
+    handleSelectAllClick(e) {
+        var allCows = this.state.availableCows;
+        allCows = allCows.concat(this.state.selectedCows)
+
+        this.setState({
+            availableCows: [],
+            selectedCows: allCows
+        }, this.updateSelectedCows(this.state.searchText))
+    }
+
+    handleSelectNoneClick(e) {
+        var allCows = this.state.selectedCows;
+        allCows = allCows.concat(this.state.availableCows)
+
+        this.setState({
+            availableCows: allCows,
+            selectedCows: []
+        }, this.updateSelectedCows(this.state.searchText))
+    }
+
     render() {
         return (
             <div className="select-widget">
-                <div className="current-cows">
+                <div className="current-cows" key={this.state.selectedCows.length}>
                     {this.state.selectedCows.length === 0 && 
                         <TagWidget name="No cows selected" passedKey={0}/>}
                     {this.state.selectedCows.length !== 0 && 
                         <>{this.displaySelectedCows()}</>}
                 </div>
-                <div className="cow-list-widget">
+                <div className="cow-list-widget" key={this.state.searchedCows.length}>
+                    <div className="select-buttons">
+                        <TagWidget name ={"Select all"} 
+                                            passedKey={0} 
+                                            key={this.state.enableSelectAll} 
+                                            enableSelectAll = {this.state.enableSelectAll}
+                                            enableSelectNone = {this.state.enableSelectNone}
+                                            onClick={(e) => this.handleSelectAllClick(e)}/>
+                        <TagWidget name ={"Select none"} 
+                                            passedKey={1} 
+                                            key={this.state.enableSelectNone} 
+                                            enableSelectAll = {this.state.enableSelectAll}
+                                            enableSelectNone = {this.state.enableSelectNone}
+                                            onClick={(e) => this.handleSelectNoneClick(e)}/>
+                    </div>
                     <input className="search-box" 
                             value={this.state.searchText} 
                             placeholder="Search..." 
                             onChange={event => this.handleSearchTextChange(event)}>        
                     </input>
-                    <div className="available-cows">
+                    <div className="available-cows" key={this.state.searchedCows.length}>
                         {this.state.searchedCows.length === 0 && 
-                            <h4 className="no-cows-text">No cows available</h4>}
+                            <h4 className="no-cows-text" key={this.state.searchedCows.length}>No cows available</h4>}
                         {this.state.searchedCows.length !== 0 && 
-                            <>{this.displaySearchedCows()}</>}
+                            <div key={this.state.searchedCows.length}>{this.displaySearchedCows()}</div>}
                     </div>
                 </div>
             </div>
--- src/widgets/TagWidget.js
+++ src/widgets/TagWidget.js
@@ -5,15 +5,23 @@ export default class TagWidget extends React.Component {
     constructor(props) {
         super(props)
 
-        if(this.props.name==="No cows selected")
+        if(this.props.name === "No cows selected")
             this.style = "tag-widget-single"
-        else
+        else if (this.props.name === "Select all" && this.props.enableSelectAll === 1)
+            this.style = "tag-widget-all"
+        else if (this.props.name === "Select all" && this.props.enableSelectAll === 0)
+            this.style = "tag-widget-disabled"
+        else if (this.props.name === "Select none" && this.props.enableSelectNone === 1)
+            this.style = "tag-widget-none"
+        else if (this.props.name === "Select none" && this.props.enableSelectNone === 0)
+            this.style = "tag-widget-disabled"
+        else 
             this.style = "tag-widget"
     }
 
     render() {
         return (
-            <span className={this.style} key={this.props.passedKey} onClick={this.props.onClick}>
+            <span className={this.style} onClick={this.props.onClick}>
                 <p className="tag-text">{this.props.name}</p>
             </span>
         );
